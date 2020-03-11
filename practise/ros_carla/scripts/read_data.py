fb080b2e4ccce3f522fddb930f41bd6975b51454
Manoel Manev <mani.manev@gmail.com>
2020-03-11 18:55:42 +0200

    file that is subscriber node and reads from controler_info topic and makes the user`s vehicle to move

--- practise/ros_carla/scripts/read_data.py
+++ practise/ros_carla/scripts/read_data.py
@@ -1,80 +1,66 @@
 #!/usr/bin/env python
-from __future__ import print_function
-import glob
-import os
-import sys
-
-# try:
-#     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
-#         sys.version_info.major,
-#         sys.version_info.minor,
-#         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-# except IndexError:
-#     pass
-
 import carla
-
-import rospy
 from msg_folder.msg import MyRosMsg
+from std_msgs.msg import Int16
 import pygame
-
+import controller_button
+import rospy
 import spawning_actors
-import time
+import publishing_all_information
 
-class Pushing_Buton:
-    def __init__(self,list_of_pushed=[],state_of_pushed_button=False):
-        self.date_of_pushed_button=list_of_pushed
-        self.state_of_pushed_button=state_of_pushed_button
-        self.date_of_released_button=[]
-    def set_state_of_pushed_button(self,bool_set):
-        self.state_of_pushed_button=bool_set
-    def set_list_of_pushed(self,list):
-        self.date_of_pushed_button=list
-        self.date_of_released_button=list
-    def set_list_of_released(self,list):
-        self.date_of_released_button=list
-    def get_button(self):
-        return self.state_of_pushed_button
-    def get_list(self):
-        return self.date_of_pushed_button
-    def get_pushed_time(self):
-        return self.date_of_released_button
-    def add_new_date_now(self):
-        self.date_of_pushed_button.append(time.strftime("%H"))
-        self.date_of_pushed_button.append(time.strftime("%M"))
-        self.date_of_pushed_button.append(time.strftime("%S"))
-    def add_released_time(self):
-        self.date_of_released_button.append(time.strftime("%H"))
-        self.date_of_released_button.append(time.strftime("%M"))
 
 client = carla.Client('localhost', 2000)
 client.set_timeout(10.0) 
 world=client.get_world()
-button_autopilot=Pushing_Buton()
-actor_list=world.get_actors()
+actors_list=world.get_actors()
+button_autopilot=controller_button.Button()
 autopilot=False
 control_car=carla.VehicleControl()
+vehicle_id=None
+draw=world.debug
+
+def get_car_cordinates(vehicle):
+    return vehicle.get_location()
+
+
+def drawing(cordinates,drawing):
+    drawing.draw_point(cordinates,color=carla.Color(0,255,0))
+    
+# Function that destroys every actor that I have made.
+def destroying_actors(world):
+    list_of_actors=world.get_actors().filter("vehicle*")
+    if len(list_of_actors)>0:
+        spawning_actors.destroyingActors(list_of_actors)
+    list_of_actors=world.get_actors().filter("sensor*")
+    if len(list_of_actors)>0:
+        spawning_actors.destroyingActors(list_of_actors)
+
+# Function that is called with the message as the first argument, when the subscriber receives new messages.
 def callback(data):
     global autopilot
     global button_autopilot
-    vehicle=actor_list.find(data.vehicle_id)
+    global vehicle_id
+    global world
+    global draw
+
+    vehicle=world.get_actors().find(vehicle_id)
     if data.is_exit==True:
         print("The console stoped :)")
+        destroying_actors(world)
         rospy.signal_shutdown("exit")
 
     if data.is_autopilot==False:
-        if len(button_autopilot.get_pushed_time()) == 0 and len(button_autopilot.get_list()) != 0:
+        if len(button_autopilot.get_when_the_button_is_released()) == 0 and len(button_autopilot.get_when_the_button_is_pushed()) != 0:
             button_autopilot.add_released_time()
             
     if data.is_autopilot==True:
-        if len(button_autopilot.get_list())!=0 and len(button_autopilot.get_pushed_time())!=0:
-                button_autopilot.set_list_of_pushed([])
-                button_autopilot.set_list_of_released([])
-        if len(button_autopilot.get_list())==0:       
-            button_autopilot.add_new_date_now()
-            button_autopilot.set_state_of_pushed_button(True)
+        if len(button_autopilot.get_when_the_button_is_pushed())!=0 and len(button_autopilot.get_when_the_button_is_released())!=0:
+                button_autopilot.set_date_of_pushed_and_released([])
+        if len(button_autopilot.get_when_the_button_is_pushed())==0:       
+            button_autopilot.add_pushed_time()
+            button_autopilot.set_state_of_button(True)
 
-        if button_autopilot.get_button() is True:
+        if button_autopilot.get_state_of_button() is True:
             if autopilot is False:
                 vehicle.set_autopilot(True)
                 autopilot=True
@@ -83,17 +69,26 @@ def callback(data):
                 autopilot=False
                 vehicle.set_autopilot(False)
                 print("AUTOPILOT OFF")
-            button_autopilot.set_state_of_pushed_button(False)
+            button_autopilot.set_state_of_button(False)
     if autopilot == False:
         control_car.reverse=data.is_reverse
         control_car.brake=data.breaks
         control_car.throttle=data.throttle
         control_car.steer=data.steer
         vehicle.apply_control(control_car)
+    print(vehicle.get_transform().rotation)
+    # drawing(get_car_cordinates(vehicle),draw)
+
+# Function that gets the id on the echo vehicle - the car that the user's controlling.
+def getInt(data):
+    global vehicle_id
+    vehicle_id=data.data
+
+# Function that initializes that this script is a subscriber.
 def listener():
- 
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("controler_info", MyRosMsg, callback)
+    rospy.Subscriber('/vehicle_id',Int16,getInt)
     rospy.spin()
 
 if __name__ == '__main__':
