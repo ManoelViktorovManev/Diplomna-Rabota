cfe8631517b312f8ac95b3c7b1eaad0f2fe18797
Manoel Manev <mani.manev@gmail.com>
2020-03-23 00:05:20 +0200

    File that publishes the input from controller into MyRosMsg

--- practise/ros_carla/scripts/ros_control.py
+++ practise/ros_carla/scripts/ros_control.py
@@ -3,14 +3,7 @@ import rospy
 import pygame
 import math
 from msg_folder.msg import MyRosMsg
-from std_msgs.msg import Int16
-import carla
 
-vehicle_id=0
-
-def getInt(data):
-    global vehicle_id
-    vehicle_id=data.data
 
 # Function that shows how to use the controller to control the car in the simulator.
 def print_info():
@@ -34,36 +27,22 @@ def is_joystick_pluged():
 
 # Function that initializes the controller. 
 def initialization_of_controller():
-    pygame.init()
-    listofall=[pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-    controller=listofall[0]
-    controller.init()
-    return controller
-
+    try:
+        pygame.init()
+        listofall=[pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+        controller=listofall[0]
+        controller.init()
+        return controller
+    except IndexError:
+            return
 # Main function. It discribes that this script is Publisher, that it will publish what the user is typing in the controller.
 def main():
-    client = carla.Client('localhost', 2000)
-    client.set_timeout(10.0) 
-    world=client.get_world()
     rospy.init_node('ros_carla_control',anonymous=True)
     pub_msg=rospy.Publisher("controler_info",MyRosMsg,queue_size=1)
-    rospy.Subscriber('/vehicle_id',Int16,getInt)
     controler=initialization_of_controller()
     print_info()
     msg=MyRosMsg()
     while True:
-        """ 
-            float32 throttle
-            float32 steer
-            float32 breaks
-            bool is_reverse
-            bool is_autopilot
-            bool is_exit
-            uint16 vehicle_id
-            string vehicle_name
-        """
-        if is_joystick_pluged() is False:
-            break
         pygame.event.get()
         msg.throttle=0
         msg.steer=0
@@ -86,8 +65,6 @@ def main():
                 msg.breaks=controler.get_axis(5)
         msg.throttle=math.fabs(controler.get_axis(1))
         msg.steer=controler.get_axis(3)
-        # msg.vehicle_id=vehicle_id
-        # msg.vehicle_name=world.get_actors().find(vehicle_id).type_id
         pub_msg.publish(msg)
         if controler.get_button(0)==True:
             break
