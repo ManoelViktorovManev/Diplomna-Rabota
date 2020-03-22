9de158b47065499a512f09c57627b75ef3314555
Manoel Manev <mani.manev@gmail.com>
2020-03-22 10:27:00 +0200

    Fixed the steer

--- practise/ros_carla/scripts/PID_Controller.py
+++ practise/ros_carla/scripts/PID_Controller.py
@@ -1,5 +1,7 @@
 import math
 import carla
+import numpy as np
+from collections import deque
 
 
 class Controller:
@@ -14,83 +16,55 @@ class Controller:
         self.breaks=0
         
     def set_speed_on_car_and_steer(self,wp):
-        self.pid_steer.set_waypoint(wp)
         self.pid_throttle.set_currnet_speed()
-        # if self.pid_steer.get_diference_between_vehicle_and_wp() > 10:
-        #     self.pid_throttle.set_wanted_speed(10)
-        # else:
-            # self.pid_throttle.set_wanted_speed(20)
-        # self.pid_throttle.set_wanted_speed(20)
         self.throttle=self.pid_throttle.moving(wp)
-        self.steer=self.pid_steer.set_steer()
+        self.steer=self.pid_steer.run_step(wp)
         self.breaks=self.pid_throttle.breaks
    
-# 1 zawoi kum posokata na wp
-# 2 zawoi kum wp
 class PID_Controller_Steer:
-    def __init__(self,vehicle):
-        self.vehicle=vehicle
-        self.waypoint=None
-        self.steer=0
 
-    def set_waypoint(self,wp):
-        self.waypoint=wp
+    def __init__(self, vehicle, K_P=1.0, K_D=0.0, K_I=0.0, dt=0.03):
 
-    def get_diference_between_vehicle_and_wp(self):
-        if self.waypoint is not None:
-            wp_angle=self.waypoint.transform.rotation.yaw
-            if wp_angle < 0:
-                wp_angle=360-math.fabs(wp_angle)
-            
-            vehicle_angle=self.vehicle.get_transform().rotation.yaw
-            if vehicle_angle < 0:
-                vehicle_angle=360-math.fabs(vehicle_angle)
+        self.vehicle = vehicle
+        self.K_P = K_P
+        self.K_D = K_D
+        self.K_I = K_I
+        self.dt = dt
+        self.e_buffer = deque(maxlen=10)
 
-            right_angle=left_angle=0
-            if min(vehicle_angle,wp_angle) is wp_angle:
-                right_angle=vehicle_angle-wp_angle
-                left_angle=-(360-right_angle)
-            else:
-                left_angle=-(wp_angle-vehicle_angle)
-                right_angle=360-(-left_angle)
-            if math.fabs(right_angle)<math.fabs(left_angle):
-                return right_angle
-            else:
-                return left_angle
-        return None
 
-    def distance_vehicle(self,waypoint):
-        loc = self.vehicle.get_location()
-        dx = waypoint.transform.location.x - loc.x
-        dy = waypoint.transform.location.y - loc.y
-        return math.sqrt(dx * dx + dy * dy)
-    
-    def get_angle_to_the_wp(self):
-        loc=self.vehicle.get_location()
-        dx=math.fabs(self.waypoint.transform.location.x - loc.x)
-        dy=math.fabs(self.waypoint.transform.location.y - loc.y)
-        if loc.y>self.waypoint.transform.location.y:
-            if loc.x > self.waypoint.transform.location.x:
-                return dy/dx
-            return -dy/dx
+    # This function is used from ~/<carla_folder>/PythonAPI/carla/agents/navigation/controller.py
+    def pid_control(self, waypoint, vehicle_transform):
+        v_begin = vehicle_transform.location
+        v_end = v_begin + carla.Location(x=math.cos(math.radians(vehicle_transform.rotation.yaw)),
+                                         y=math.sin(math.radians(vehicle_transform.rotation.yaw)))
+        v_vec = np.array([v_end.x - v_begin.x, v_end.y - v_begin.y, 0.0])
+        w_vec = np.array([waypoint.transform.location.x -
+                          v_begin.x, waypoint.transform.location.y -
+                          v_begin.y, 0.0])
+        dot = math.acos(np.clip(np.dot(w_vec, v_vec) /
+                                 (np.linalg.norm(w_vec) * np.linalg.norm(v_vec)), -1.0, 1.0))
+
+        cross = np.cross(v_vec, w_vec)
+        if cross[2] < 0:
+            dot *= -1.0
+
+        self.e_buffer.append(dot)
+        de=0
+        ie=0
+        if len(self.e_buffer) >= 2:
+            de = (self.e_buffer[-1] - self.e_buffer[-2]) / self.dt
+            ie = sum(self.e_buffer) * self.dt
         else:
-            if loc.x > self.waypoint.transform.location.x:
-                return -dx/dy  
-            return  dx/dy
-    
-    def set_steer(self):
-        # angle=self.get_diference_between_vehicle_and_wp()
-        # print(angle)
-        angle=self.get_angle_to_the_wp()
-        # if angle is not None:
-        angle=angle*180/3.14
-        # + ili - ?   
-        if math.fabs(angle)<=2:
-            return 0
-        else:
-            return 1.5*angle/180
+            de = 0.0
+        
+        return np.clip((self.K_P * dot) + (self.K_D * de /
+                                             self.dt) + (self.K_I * ie * self.dt), -1.0, 1.0)
 
-# steer - zawoi naso4wane
+    def run_step(self, waypoint):
+        return self.pid_control(waypoint, self.vehicle.get_transform())
+
+
 class PID_Controller_Throttle:
     def __init__(self,vehicle,wanted_speed=20):
         self.vehicle=vehicle
@@ -102,12 +76,10 @@ class PID_Controller_Throttle:
     def set_wanted_speed(self,wanted):
         self.wanted_speed=wanted
     
-
     def get_speed(self):
         vel = self.vehicle.get_velocity()
         return 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
 
-        
     def set_currnet_speed(self):
         self.current_speed=self.get_speed()
         
@@ -123,12 +95,8 @@ class PID_Controller_Throttle:
         if self.wanted_speed is 0:
            self.breaks=1
         difference=self.get_speed()-self.wanted_speed
-        # print(self.get_speed())
         val=self.get_speed_val(difference)
         return val
 
-
-
     def moving(self,waypoint):
-        return self.controll_speed()
-        
+        return self.controll_speed()        
