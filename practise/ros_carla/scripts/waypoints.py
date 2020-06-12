47b8c1566edef299fb68cc1c80e15fb6dbcba2fe
Manoel <mani.manev@gmail.com>
2020-06-13 00:30:09 +0300

    Class that create a path

--- practise/ros_carla/scripts/waypoints.py
+++ practise/ros_carla/scripts/waypoints.py
@@ -1,26 +1,27 @@
 import time
 import carla 
-import sys
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
 import random
-import PID_Controller
+import graph
 
-
-class Createing_Road:
-    
+class Creating_Path:
     def __init__(self,vehicle):
+
         self.vehicle=vehicle
         self.world=self.vehicle.get_world()
         self.draw=self.world.debug
         self.map=self.world.get_map()
         self.all_waypoints=self.map.generate_waypoints(2.0)
         self.length=0
-        self.graph=nx.DiGraph()
+        self.graph=graph.Graph()
         self.path=None
         self.waypoints_from_the_path=[]
         self.current_waypoint=None
+        self.emergency_waypoints=[]
+        self.num_to_pass=2
+        self.remember_wp_to_follow=None
 
         # Function to call to make graph :)
         self.making_nodes_only_from_crossroad()
@@ -34,14 +35,14 @@ class Createing_Road:
 
     # Function that returns the number of an element in the graph that is with the same value as wp. 
     def get_element_from_graph(self,wp):
-        for i in list(self.graph.nodes):
+        for i in self.graph.nodes:
             if self.compare_waypoints_instance(i,wp) is True:
                 return i
         return None
         
     # Function that draws a waypoint.
-    def drawing_waypoint(self,waypoint):
-        self.draw.draw_point(waypoint.transform.location)
+    def drawing_waypoint(self,waypoint,color):
+        self.draw.draw_point(waypoint.transform.location,color=carla.Color(r=color[0],g=color[1],b=color[2]))
 
     # Function that compares two waypoints.
     def compare_waypoints_instance(self,wp1,wp2):
@@ -52,9 +53,7 @@ class Createing_Road:
         return math.fabs(wp1.transform.location.x-wp2.transform.location.x) + math.fabs(wp1.transform.location.y-wp2.transform.location.y) 
         + math.fabs(wp1.transform.location.z-wp2.transform.location.z) + math.fabs(wp1.transform.rotation.pitch-wp2.transform.rotation.pitch)
         + math.fabs(wp1.transform.rotation.yaw-wp2.transform.rotation.yaw) + math.fabs(wp1.transform.rotation.roll-wp2.transform.rotation.roll)
-        # FUNTIATA DA YA OPRAWA
 
-    # ?????????
     def compare_waypoint_and_vehicle(self,wp):
         vehicle=self.vehicle.get_transform()
         return math.fabs(wp.transform.location.x-vehicle.location.x) + math.fabs(wp.transform.location.y-vehicle.location.y) 
@@ -68,7 +67,7 @@ class Createing_Road:
     def getting_closest_one_wp(self,wp):
         min_value=float("inf")
         waypoint=wp
-        for i in list(self.graph.nodes):
+        for i in self.graph.nodes:
             value = math.fabs(i.transform.location.x-wp.transform.location.x) + math.fabs(i.transform.location.y-wp.transform.location.y) 
             + math.fabs(i.transform.location.z-wp.transform.location.z) + math.fabs(i.transform.rotation.pitch-wp.transform.rotation.pitch)
             + math.fabs(i.transform.rotation.yaw-wp.transform.rotation.yaw) + math.fabs(i.transform.rotation.roll-wp.transform.rotation.roll)
@@ -77,22 +76,6 @@ class Createing_Road:
                 min_value=value
                 waypoint=i
         return waypoint
-        # POWTARYA SE SUS DOLNATA FUNKCIYA
-
-    # Function that returns closest waypoint to the car 
-    def getting_closest_one_wp_from_car(self,cordinated):
-        min_value=float("inf")
-        waypoint=cordinated
-        for i in list(self.graph.nodes):
-            value = math.fabs(i.transform.location.x-cordinated.location.x) + math.fabs(i.transform.location.y-cordinated.location.y) 
-            + math.fabs(i.transform.location.z-cordinated.location.z) + math.fabs(i.transform.rotation.pitch-cordinated.rotation.pitch)
-            + math.fabs(i.transform.rotation.yaw-cordinated.rotation.yaw) + math.fabs(i.transform.rotation.roll-cordinated.rotation.roll)
-            if min_value > value:
-                min_value=value
-                waypoint=i
-        return waypoint
-        # POWTARYA SE SUS GORNATA FUNKCYTA
-
 
     # Function that returns list of waypoints whcih conect the value waypoint
     def connected_nodes_from_crossroad(self,waypoint):
@@ -131,7 +114,7 @@ class Createing_Road:
 
     # Function that connects nodes from the graph
     def making_graph_from_crossroad(self):
-        for i in list(self.graph.nodes()):
+        for i in self.graph.nodes:
             crossroad=self.connected_nodes_from_crossroad(i)
             for k in crossroad:
                 node=self.getting_closest_one_wp(k)
@@ -147,12 +130,12 @@ class Createing_Road:
     
     # Function that chooses ranndom node from graph 
     def choose_random(self):
-        return random.choice(list(self.graph.nodes()))
+        return random.choice(self.graph.nodes)
 
     # Funtcion that prints the path 
-    def printing_path(self):
+    def printing_path(self,color=[255,0,0]):
         for i in self.waypoints_from_the_path:
-            self.drawing_waypoint(i)
+            self.drawing_waypoint(i,color)
             time.sleep(0.0001)
 
     def getting_all_waypoints_from_node_to_node(self):
@@ -160,7 +143,7 @@ class Createing_Road:
         for i in range(len(self.path[:-1])):
             arr1=self.path[i]
             arr2=self.path[i+1]
-            asdf=self.graph.get_edge_data(self.get_element_from_graph(arr1),self.get_element_from_graph(arr2))['data']
+            asdf=self.graph.get_edge_data(self.get_element_from_graph(arr1),self.get_element_from_graph(arr2))
             list.append(asdf)
         return list
 
@@ -174,7 +157,6 @@ class Createing_Road:
     def get_closest_wp_to_the_car(self):
         return self.map.get_waypoint(self.vehicle.get_location())
 
-
     def get_wp_to_crossroad(self,waypoint,list=[]):
         next_wp=waypoint.next(2.0)
         if len(next_wp)>1:
@@ -184,12 +166,72 @@ class Createing_Road:
         waypoint=self.getting_waypoint(next_wp)
         return self.get_wp_to_crossroad(waypoint,list)
 
-    def create_road(self,remain_waypoints=[]):
-        if len(remain_waypoints) == 0:
+    
+    def recreate_graph(self):
+        new_graph=nx.DiGraph()
+        for i in self.graph.nodes:
+            new_graph.add_node(i)
+
+        for element in self.graph.edges:
+            for i in self.graph.edges[element]:
+                new_graph.add_edge(element,i[0],length=i[1],data=i[2])
+
+        return new_graph
+
+
+    def create_path_to_closest_crossroad(self):
+        if len(self.waypoints_from_the_path)!=0:
+            self.waypoints_from_the_path = self.get_wp_to_crossroad(self.waypoints_from_the_path[-1].next(2.0)[0],self.waypoints_from_the_path)
+        else:
             close_one_wp_to_car=self.get_closest_wp_to_the_car()
-            waypoints_to_crossroad=self.get_wp_to_crossroad(close_one_wp_to_car)
-            remain_waypoints=waypoints_to_crossroad    
-        
-        self.path=nx.shortest_path(self.graph,self.getting_closest_one_wp_from_car(remain_waypoints[-1].transform),self.choose_random())
+            self.waypoints_from_the_path=self.get_wp_to_crossroad(close_one_wp_to_car,[]) 
+
+
+    def create_path(self):
+        self.path=self.graph.dijkistra(self.getting_closest_one_wp_from_car(self.waypoints_from_the_path[-1].transform),self.choose_random())
+        if self.path == None:
+            self.create_path_to_closest_crossroad()
+            self.create_path()
+            return
         waypoints= self.converting_the_path_to_list_of_waypoints(self.getting_all_waypoints_from_node_to_node())
-        self.waypoints_from_the_path=remain_waypoints+waypoints
+        self.waypoints_from_the_path=self.waypoints_from_the_path+waypoints
+        self.get_starting_waypoints()
+        self.emergency_waypoints=self.waypoints_from_the_path[:]
+
+    def choose_index(self):
+        vel = self.vehicle.get_velocity()
+        speed= 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
+        if speed > 30:
+            return int(speed/10) 
+        return 0
+        
+        
+    def get_starting_waypoints(self):
+        number=self.choose_index()
+        self.waypoints_from_the_path=self.waypoints_from_the_path[number:]
+
+    def refresh_call(self):
+        self.path=None
+        self.waypoints_from_the_path=[]
+        self.current_waypoint=None
+        self.emergency_waypoints=[]
+        self.num_to_pass=2
+
+    def compare_waypoints_rotations(self,wp1,wp2):
+        return math.fabs(wp1.transform.rotation.yaw-wp2.transform.rotation.yaw)
+        + math.fabs(wp1.transform.rotation.pitch-wp2.transform.rotation.pitch)
+        + math.fabs(wp1.transform.rotation.roll-wp2.transform.rotation.roll)
+
+    def emergency_wp(self,current_speed):
+        nums=int(round(current_speed/10.0))
+        self.remember_wp_to_follow=self.waypoints_from_the_path[nums-2]
+
+            
+    def create_emargance_waypoints(self,current_speed=0):
+        nums=int(round(current_speed/10.0))
+        if nums!=self.num_to_pass and nums>=2:
+            self.num_to_pass=nums
+    
+    def refresh_waypoints(self):
+        self.waypoints_from_the_path=self.emergency_waypoints[self.emergency_waypoints.index(self.waypoints_from_the_path[0]):]
+        self.emergency_waypoints=self.waypoints_from_the_path[:]
